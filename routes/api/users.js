4364389db64f9aa6ca345fadaae1562e75a51247
bushbass <alex.nielsen@gmail.com>
2018-07-05 12:35:06 -0400

    I thought I only changed my config file---?

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+// Load user model
+const User = require("../../models/User");
+
 // @route  GET api/users/test
 // @desc   Tests users route
 // @access Public
 router.get("/test", (req, res) => res.json({ message: "Users Works" }));
+
+// @route  GET api/users/register
+// @desc   register a user
+// @access Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
