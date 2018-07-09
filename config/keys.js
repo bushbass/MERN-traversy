22c8d3816a6bbc502acab64baa8886aa4b24ad25
bushbass <alex.nielsen@gmail.com>
2018-07-09 09:14:11 -0400

    deleted db password because gitignore wouldn't work

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   monguURI:
-    "mongodb://bushbass:bd3snd@ds123181.mlab.com:23181/traversy-mern-front-to-back"
+    "mongodb://bushbass:<password>@ds123181.mlab.com:23181/traversy-mern-front-to-back"
 };
