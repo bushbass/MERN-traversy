2fec88a600f0db4453bb15c526199453196479e5
bushbass <alex.nielsen@gmail.com>
2018-07-09 17:15:40 -0400

    finisehd video 11 I think?

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Load user model
 const User = require("../../models/User");
@@ -17,14 +20,53 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "email already exists" });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", // rating
+        d: "mm" // default
+      });
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar,
         password: req.body.password
       });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
   });
 });
 
+// @route  GET api/users/login
+// @desc   login user / returning JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    }
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ message: "success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
 module.exports = router;
